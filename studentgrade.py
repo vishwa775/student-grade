5f251c8a9917506f4e948494a4fa317d30ef62c5
vishwa775 <vishwaamagi@gmail.com>
2025-10-23 16:32:28 +0530

    save

---
+++ studentgrade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter marks of the student: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Grade:", grade)
